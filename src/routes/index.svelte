1263d8cb11870c4e177e160cf44ac73315be40ed
Kacper Malek <kacper.malek0828@gmail.com>
2022-06-29 17:48:22 +0200

    feat: import and use HeroSection, MenuSection, AboutSection, and set head title to Feane Restaurant

---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script>
+  import HeroSection from "@/components/HeroSection.svelte";
+  import MenuSection from "@/components/MenuSection.svelte";
+  import AboutSection from "@/components/AboutSection.svelte";
+</script>
+
+<svelte:head>
+  <title>Feane Restaurant</title>
+</svelte:head>
+
+<HeroSection />
+<MenuSection />
+<AboutSection />
