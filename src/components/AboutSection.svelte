789b986f052945895c97773db055247a070b18e6
Kacper Malek <kacper.malek0828@gmail.com>
2022-06-29 17:49:12 +0200

    feat: add AboutSection component

---
+++ src/components/AboutSection.svelte
@@ -0,0 +1,28 @@
+<div class="w-full h-[750px] bg-slate-800">
+  <div class="w-4/5 2xl:w-3/5 h-full mx-auto">
+    <div class="grid grid-cols-2 h-full">
+      <div class="h-full flex flex-col justify-center">
+        <img src="/images/about-img.png" alt="About Section Burger" />
+      </div>
+
+      <div class="h-full flex flex-col justify-center ml-12">
+        <div>
+          <h2 class="text-5xl text-bold font-brand text-white">We Are Feane</h2>
+          <p class="text-white mt-4">
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable. If you are going to use a passage of Lorem Ipsum, you
+            need to be sure there isn't anything embarrassing hidden in the
+            middle of text. All
+          </p>
+
+          <button
+            class="px-6 py-2 mt-8 text-white bg-yellow-400 hover:bg-yellow-600 rounded-3xl transition-colors duration-700 ease-out"
+            >Read More</button
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
