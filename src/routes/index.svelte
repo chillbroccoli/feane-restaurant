1b21e7092e38e6037b1d2054d935c2185b400e10
Kacper Malek <kacper.malek0828@gmail.com>
2022-06-30 12:22:24 +0200

    refactor: import and add new sections

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,9 @@
   import HeroSection from "@/components/HeroSection.svelte";
   import MenuSection from "@/components/MenuSection.svelte";
   import AboutSection from "@/components/AboutSection.svelte";
+  import BookSection from "@/components/BookSection.svelte";
+  import TestimonialsSection from "@/components/TestimonialsSection.svelte";
+  import FooterSection from "@/components/FooterSection.svelte";
 </script>
 
 <svelte:head>
@@ -11,3 +14,6 @@
 <HeroSection />
 <MenuSection />
 <AboutSection />
+<BookSection />
+<TestimonialsSection />
+<FooterSection />
