ad23a356e392f7f5fc444dda7d634e8cbfd53b8b
Kacper Malek <kacper.malek0828@gmail.com>
2022-07-01 15:14:57 +0200

    refactor: adjust FooterSection for mobile phones

--- src/components/FooterSection.svelte
+++ src/components/FooterSection.svelte
@@ -11,7 +11,7 @@
 
 <div class="w-full bg-slate-800">
   <div class="w-4/5 2xl:w-3/5 h-full mx-auto py-10">
-    <div class="grid grid-cols-3 gap-x-8">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-x-8 gap-y-8 md:gap-y-0">
       <div>
         <h3 class="text-white text-4xl font-brand text-center">Contact Us</h3>
         <ul class="text-white mt-4">
